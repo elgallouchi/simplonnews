6bc7960c9948a3b27a0050b133356539619d37ce
Esther V <esvalas@gmail.com>
2021-02-10 10:47:42 +0100

    integration ok page index

--- static/js/index.js
+++ static/js/index.js
@@ -1,11 +1,9 @@
+// SWITCH page connexion à page inscription
+
 let boutonInscription = document.getElementById('inscription')
 let boxRegister = document.querySelector('.container-form-register')
 let boxConnexion = document.querySelector('.container-form-connexion')
 let boutonConnexion = document.getElementById('connexion')
-
-console.log(boutonConnexion)
-
-
 
 boutonInscription.addEventListener('click', () => {
     boxConnexion.style.display = "none";
