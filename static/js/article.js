f844044fd995be439e3c5fa26fbf9f1243f0cc8a
rozaribeiro <rozaribeiro@gmail.com>
2021-07-02 14:06:22 +0200

    ma version

--- static/js/article.js
+++ static/js/article.js
@@ -1,49 +1,89 @@
-// VERIFICATION du token
+// ******************** verification du token ******************** //
 
 let tokenStorage = sessionStorage.getItem('token');
 if (!tokenStorage) {
     window.location.href = "/index.html"
 }
 
-function generateAside(asideView) {
-    let newAside = document.querySelector("aside");
-    let outputAside = '';
-    
-    asideView.forEach(article => {
-        outputAside += `
-        <a href="./article.html?id=${article.id}"><img id="asideImg" src="${article.img}" alt="${article.title}"></a>
-        <a href="./article.html?id=${article.id}"><h4 id="asideTitle">${article.title}</h4></a>
-        `;
-    });
-    newAside.innerHTML = outputAside;
-}
+// ******************** declara manip dom ************************ //
+
+var articleTitle = document.getElementById("articleTitle");
+var articleResume = document.getElementById("articleResume");
+var articleText = document.getElementById("articleText");
+var articleAuthor = document.getElementById("articleAuthor");
+var articleImg = document.querySelector("article > div");
+var articleImg = document.querySelector("article > div");
+var btsNextPrev = document.querySelector(".btsNextPrev");
+
+// ******************** autres variables ************************ //
+
+let lienCurrent = window.location.href;
+let lienId = lienCurrent.split("id=")[1]
+
+
+// ******************** function generateArticle ************************ //
 
 function generateArticle(artArray) {
     
     var url = window.location.search;
     var splitUrl = url.split("=");
     var idChoix = parseInt(splitUrl[1]);
- 
+    
     var indexChoix = artArray.map(function(x) {return x.id; }).indexOf(idChoix);
     var objChoisi = artArray[indexChoix];
     
-    // console.log(idChoix);
-    // console.log(indexChoix);
-    // console.log(objChoisi);
-
-    var articleTitle = document.getElementById("articleTitle");
-    var articleResume = document.getElementById("articleResume");
-    var articleText = document.getElementById("articleText");
-    var articleAuthor = document.getElementById("articleAuthor");
-    var articleImg = document.querySelector("article > div");
-
     articleTitle.textContent = objChoisi.title;
     articleResume.textContent = objChoisi.resume; 
     articleImg.innerHTML = `<img src=${objChoisi.img} alt=${objChoisi.title}>`;
     articleText.textContent = objChoisi.content;
     articleAuthor.textContent = objChoisi.author;
+    
+    let nextIndex = "";
+    let prevIndex = "";
+
+    function createBtsNextPrev(i){
+        if (i == 0){
+            nextIndex = 1;
+            prevIndex = artArray.length-1;
+       }
+        else if (i == artArray.length-1){
+            nextIndex = 0;
+            prevIndex = artArray.length-2;
+        }   
+        else{
+            nextIndex = indexChoix+1;
+            prevIndex = indexChoix-1;
+        }
+    }
+    createBtsNextPrev(indexChoix);
+
+    let nextId = artArray[nextIndex].id;
+    let prevId = artArray[prevIndex].id;
+
+    btsNextPrev.innerHTML = `<a href="./article.html?id=${prevId}">&laquo; Article précédant</a>
+    <a href="./article.html?id=${nextId}">Article suivant &raquo;</a>`
+
 }
 
+// ******************** function generateAside ************************ //
+
+function generateAside(asideView) {
+
+    let newAside = document.querySelector("aside");
+    let outputAside = '';
+    let count = 0;
+
+    asideView.forEach(article => {
+        if (count < 5) {
+            outputAside += `
+            <a href="./article.html?id=${article.id}"><img id="asideImg" src="${article.img}" alt="${article.title}"></a>
+            <a href="./article.html?id=${article.id}"><h4 id="asideTitle">${article.title}</h4></a>
+            `;
+            count += 1;
+        }
+    })
+    newAside.innerHTML = outputAside;
+}
 fetch("https://simplonews.brianboudrioux.fr/articles", {
     method: "GET",
     headers: {
@@ -54,16 +94,14 @@ fetch("https://simplonews.brianboudrioux.fr/articles", {
         response.json()
             .then(function (response) {
                 if (response.status === 400) {
-                    // window.location.href = "/kilk";
                 } else if (response.status === 403) {
-                    // window.location.href = "/link";
                 } else {
                     let arrayReversed = response.articles.reverse();
                     if (!arrayReversed) {
                         document.querySelector('.container').innerHTML = "attent !";
                     }
-                    generateArticle(response.articles);
                     generateAside(response.articles);
+                    generateArticle(response.articles.reverse());
                 }
             })
     })
--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,14 @@
+let tokenStorage = sessionStorage.getItem('token');
+if (tokenStorage) {
+    window.location.href = "./static/views/home.html"
+} 
+
+
+let logo = document.querySelector('.logo > a > img');
+const logoSimplon = './static/img/logo_simplon_news.png';
+
+logo.src = logoSimplon
+
 // SWITCH page connexion à page inscription
 
 let boutonSInscrire = document.getElementById('inscription')
@@ -17,78 +28,69 @@ boutonConnexion.addEventListener('click', () => {
 
 
 // Mot de passe oublié
-
-let forgotPassword = document.getElementById('forgot')
+let forgotPassword = document.getElementById('forgot');
 
 forgotPassword.addEventListener('click', () => {
     prompt('Entrez votre adresse email pour recevoir un lien de réinitialisation de votre mot de passe');
-    
+
 })
 
 // AUTHENTIFICATION user
 
-
 let inputSubmit = document.querySelector("[name=valider-connexion]");
-
-
-inputSubmit.addEventListener('click', function() {
+inputSubmit.addEventListener('click', function () {
     let inputEmail = document.querySelector("[name=email-connexion]").value;
-    let inputPassword = document.querySelector("[name=password-connexion]").value;    
+    let inputPassword = document.querySelector("[name=password-connexion]").value;
     connexion(inputEmail, inputPassword)
+    validateForm([inputEmail, inputPassword])
 })
 
 function connexion(email, password) {
 
-let fetch_config = {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-        "email": email,
-        "password": password,
-    })
-}
-
-fetch("https://simplonews.brianboudrioux.fr/users/login", fetch_config)
-.then(function (response) {
-        response.json()
-        .then(function (data) {
-            if (response.status == 400) {
-                alert('Entrer un email et un mot de passe valides')
-            }
-            else {
-                console.log('OK')
-                
-                let token = data.token;
-                console.log('token')
-
-                sessionStorage.setItem('token', token);
-
-                window.location.href = "./static/views/home.html"
-            }  
+    let fetch_config = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "email": email,
+            "password": password,
         })
-        .catch(function(error){
-            console.log(error)
+    }
+
+    fetch("https://simplonews.brianboudrioux.fr/users/login", fetch_config)
+        .then(function (response) {
+            response.json()
+                .then(function (data) {
+                    if (response.status == 400) {
+                        alert('Entrer un email et un mot de passe valides')
+                    }
+                    else {
+                        console.log('bien connecte');
+                        console.log(data);
+                        let token = data.token;
+                        // console.log('token')
+                        // console.log(data);
+                        sessionStorage.setItem('token', token);
+                        window.location.href = "./static/views/home.html"
+                    }
+                })
+                .catch(function (error) {
+                    console.log(error)
+                })
         })
-
-})
-.catch(function (errors) {
-
-})
-
+        .catch(function (errors) {
+        })
 }
 
 // INSCRIPTION New user
 
 let boutonInscription = document.querySelector("[name=bouton-inscription]")
-
-boutonInscription.addEventListener('click', function() {
+boutonInscription.addEventListener('click', function () {
     let prenomInscription = document.querySelector("[name=prenom-inscription]").value
     let nomInscription = document.querySelector("[name=nom-inscription]").value
     let emailInscription = document.querySelector("[name=email-inscription]").value
     let passwordInscription = document.querySelector("[name=password-inscription]").value
-   
     inscriptionNewUser(prenomInscription, nomInscription, emailInscription, passwordInscription)
 })
 
@@ -105,44 +107,43 @@ function inscriptionNewUser(prenom, nom, email, password) {
             "password": password,
         })
     }
-    
+
     fetch("https://simplonews.brianboudrioux.fr/users", fetch_config2)
-    .then(function (response) {
-        
+        .then(function (response) {
             console.log('success');
             response.json()
-            .then(function(data) {
-                if (response.status == 400) {
-                    console.log('data');
-                }
-                else {
-                    console.log(data);
-                    console.log("utilisateur inscrit")
-                    boxRegister.style.display = "none";
-                    boxConnexion.style.display = "flex";
-                }  
-            })
-            .catch(function(error){
-                console.log(error)
-            })
-        
-    })
-    .catch(function (errors) {
-    
-    })
+                .then(function (data) {
+                    if (response.status == 400) {
+                        console.log('erreur 400');
+                    }
+                    else {
+                        console.log(data);
+                        console.log("utilisateur inscrit")
+                        boxRegister.style.display = "none";
+                        boxConnexion.style.display = "flex";
+                    }
+                })
+                .catch(function (error) {
+                    console.log(error)
+                })
+
+        })
+        .catch(function (errors) {
+
+        })
 }
 
 
 // Gestion d'erreurs formulaire
-
 function validateForm(input_tab) {
-    for (let i=0; i< input_tab.length; i++) {
+    for (let i = 0; i < input_tab.length; i++) {
         const element = input_tab[i];
 
-        if (element.value == null || element.value == "")
+        if (element == null || element == "") {
+            alert('Entrer un email et un mot de passe valides')
             return false;
+        }
     }
-
     return true;
 }
 
